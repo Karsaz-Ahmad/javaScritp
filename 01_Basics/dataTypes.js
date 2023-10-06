df86476e0886dad55e525215063c16b22536679d
Karsaz-Ahmad <146833015+Karsaz-Ahmad@users.noreply.github.com>
2023-10-06 06:12:23 +0000

    variables and datatypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,8 @@
+//alert(3+3) we are using nodejs, not browser
+//********** prmitives datatypes */
+//numbers => 2 to power 53
+//bigInt
+//bool
+//null =>standalone value
+//undefined=>
+//symbol
--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,9 +1,12 @@
 const accountId = 12345
 let accountEmail = "karsaz@yahoo.com"
-var = accountPassword = "123432"
+var accountPassword = "123432"
 accountCity = "Allahabad"
 let accountState;
 // accountId =3 not allowed
+/*
+prefer not to use var because of var does not understand scopes
+*/
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountState]);
  
