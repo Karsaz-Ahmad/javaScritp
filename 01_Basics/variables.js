9dffce84793f1c57ace2372daf2e43b990042cef
Karsaz-Ahmad <146833015+Karsaz-Ahmad@users.noreply.github.com>
2023-10-06 05:50:09 +0000

    somechange

---
+++ 01_Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "karsaz@yahoo.com"
+var = accountPassword = "123432"
+accountCity = "Allahabad"
+let accountState;
+// accountId =3 not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountState]);
+ 
